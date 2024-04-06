340dd9ff61b436f7d11feb643f0f6b63bc3bdc81
Dennis Nguyen <congnv8192@gmail.com>
2024-04-06 10:06:36 +0700

    'notfound'

--- app.js
+++ app.js
@@ -2,25 +2,32 @@ const express = require("express");
 
 const app = express();
 
+// serve static files
+app.use(express.static("public"));
+
 // set view engine
 app.set("view engine", "ejs");
 
 // routes
-app.get("/", (req, res) => {
+app.get("/hello", (req, res) => {
   res.render("home/index");
 });
+
 app.get("/artists", (req, res) => {
   res.render("artists/index");
 });
+
 app.get("/categories", (req, res) => {
   res.render("categories/index");
 });
+
 app.get("/search", (req, res) => {
   res.render("search");
 });
 
-// serve static files
-app.use(express.static("public"));
+app.get("*", (req, res) => {
+  res.render("not-found");
+});
 
 app.listen(3000, () => {
   console.log("Server listening on port 3000...");
